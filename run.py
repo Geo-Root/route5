07b8ad882f0fd081657ada7912ca0b91cbf13961
Alex Ferechin <alex.ferechin@gmail.com>
2016-03-03 20:25:20 +0300

    Initial commit with login page

---
+++ run.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#@created: 03.03.2016
+#@author: Alex Ferechin
+#@contact: alex.ferechin@gmail.com
+
+'''
+Wrapper for local server running.
+'''
+import sys
+import os
+from route5.settings import TEST_SERVER_PORT, WORKING_DIR
+import yaml
+
+def load_settings(file_name):
+	pass
+
+### Load settings from yaml file
+
+file_name = os.path.join("instance/settings.yaml")
+settings = load_settings(file_name)
+
+current_dir = WORKING_DIR #os.path.dirname(os.path.realpath(__file__))
+parent_dir = os.path.abspath(os.path.join(current_dir, os.pardir))
+sys.path.append(parent_dir)
+
+from route5 import app
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=TEST_SERVER_PORT)
