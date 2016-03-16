affac553367dd5537d1e8f8c7791e2d247c03220
Alex Ferechin <alex.ferechin@gmail.com>
2016-03-16 17:00:08 +0300

    mobile control

--- route5/views/profile.py
+++ route5/views/profile.py
@@ -95,8 +95,26 @@ def shipper_profile():
 
     return render_template(template_name, d=d)
 
+def business_profile():
+    d = get_context()
+    if not "user" in session:
+        return redirect(url_for("signup"))
+    template_name = "profile_%s.html" % d["user"]["user_type"]
+    if "user_image" in d["user"]:
+        d["user"]["profile_image"] = os.path.join("/static/uploads", d["user"]["user_image"]) + "?%s" % time.clock()
+    form = UserForm(request.form)
+
+    d["form"] = form
+    for key, value in d["user"].items():
+        if value and key in d["form"].data:
+            if key in d["form"].data:
+                getattr(d["form"], key).data = value
+
+    return render_template(template_name, d=d)
+
 app.add_url_rule('/profile', 'user_profile', user_profile, methods=['GET'])
-app.add_url_rule('/profile', 'shipper_profile', shipper_profile, methods=['GET'])
+app.add_url_rule('/profile', 'user_profile', shipper_profile, methods=['GET'])
+app.add_url_rule('/profile', 'business_profile', business_profile, methods=['GET'])
 app.add_url_rule('/rpc/save/user_profile', 'save_user_profile', save_user_profile, methods=['POST'])
 app.add_url_rule('/upload/profile', 'upload_profile_photo', upload_profile_photo, methods=['GET', 'POST'])
 app.add_url_rule('/rpc/me/image', 'get_my_image', get_my_image, methods=['GET'])
